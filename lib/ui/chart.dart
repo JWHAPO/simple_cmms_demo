8b8e064d05a080c7b30f2021d558b073e0e0ca2f
JWHAPO <kim2101714@gmail.com>
2019-10-23 05:18:48 +0900

    add pie Chart

--- lib/ui/chart.dart
+++ lib/ui/chart.dart
@@ -226,13 +226,91 @@ class _ChartPageState extends State<ChartPage> {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-
+                Indicator(
+                  color: Color(0xff0293ee),
+                  text: 'First',
+                  isSquare: true,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Indicator(
+                  color: Color(0xfff8b250),
+                  text: 'Second',
+                  isSquare: true,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Indicator(
+                  color: Color(0xff845bef),
+                  text: 'Third',
+                  isSquare: true,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Indicator(
+                  color: Color(0xff13d38e),
+                  text: 'Fourth',
+                  isSquare: true,
+                ),
+                SizedBox(
+                  height: 18,
+                ),
               ],
+            ),
+            const SizedBox(
+              width: 28,
             )
           ],
         ),
       ),
     );
+  }
+
+  List<PieChartSectionData> showingSections(){
+    return List.generate(4, (i){
+      final isTouched = i == pieTouchedIndex;
+      final double fontSize = isTouched ? 25 : 16;
+      final double radius = isTouched ? 60 : 50;
+      switch(i){
+        case 0:
+          return PieChartSectionData(
+            color : const Color(0xff0293ee),
+            value : 40,
+            title : '40%',
+            radius : radius,
+            titleStyle: TextStyle(fontSize: fontSize, fontFamily: 'NotoSans-bold', color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color : const Color(0xfff8b250),
+            value : 30,
+            title : '30%',
+            radius : radius,
+            titleStyle: TextStyle(fontSize: fontSize, fontFamily: 'NotoSans-bold', color: const Color(0xffffffff)),
+          );
+        case 2:
+          return PieChartSectionData(
+            color : const Color(0xff845bef),
+            value : 15,
+            title : '15%',
+            radius : radius,
+            titleStyle: TextStyle(fontSize: fontSize, fontFamily: 'NotoSans-bold', color: const Color(0xffffffff)),
+          );
+        case 3:
+          return PieChartSectionData(
+            color : const Color(0xff13d38e),
+            value : 15,
+            title : '15%',
+            radius : radius,
+            titleStyle: TextStyle(fontSize: fontSize, fontFamily: 'NotoSans-bold', color: const Color(0xffffffff)),
+          );
+        default :
+          return null;
+      }
+    });
   }
 
   Widget lineSample2(){
