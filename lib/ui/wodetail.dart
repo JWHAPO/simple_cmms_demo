7ad7e56517486687aefa1ca46e5068701a96b1f2
JWHAPO <kim2101714@gmail.com>
2019-11-04 08:22:40 +0900

    just add wo detail page

---
+++ lib/ui/wodetail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WoDetailPage extends StatefulWidget {
+  @override
+  _WoDetailPageState createState() => _WoDetailPageState();
+}
+
+class _WoDetailPageState extends State<WoDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
