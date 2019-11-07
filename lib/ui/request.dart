99142c26cbf0944f9da3bc2e8c803bb1a151c553
JWHAPO <kim2101714@gmail.com>
2019-11-08 05:26:54 +0900

    add app bar to Request Page

--- lib/ui/request.dart
+++ lib/ui/request.dart
@@ -54,77 +54,73 @@ class _RequestPageState extends State<RequestPage> {
     //사진
 
     return Material(
-      child: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is Request Page',
-              style: TextStyle(color: Colors.blue, fontSize: 32),
-            ),
-            RaisedButton(
-                child: Text(
-                  'Back!',
-                  style: TextStyle(color: Colors.blue, fontSize: 16),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0 , right: 10.0),
-              child: Stack(
-                alignment: Alignment.centerRight,
-                children: <Widget>[
-                  TextFormField(
-                    controller: _equipmentTextEditingController,
-                    focusNode: _equipmentFocusNode,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                      hintText: 'Write equipment name. ',
-                      labelText: 'Equipment *',
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.1,
+          backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
+          title: Text('Request', style: TextStyle(fontFamily: 'NotoSans-bold'),),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 10.0 , right: 10.0),
+                child: Stack(
+                  alignment: Alignment.centerRight,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _equipmentTextEditingController,
+                      focusNode: _equipmentFocusNode,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                        hintText: 'Write equipment name. ',
+                        labelText: 'Equipment *',
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 5.0),
-                    child: InkWell(
-                      onTap: (){
-                        scanBarcode();
-                      },
-                      child: Icon(Icons.filter_center_focus, size: 30,),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: InkWell(
+                        onTap: (){
+                          scanBarcode();
+                        },
+                        child: Icon(Icons.filter_center_focus, size: 30,),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              width: 320,
-              height: 120,
-              alignment: Alignment.center,
-              color: Colors.grey,
-              padding: EdgeInsets.all(10.0),
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: _image == null ? Icon(Icons.add, color: Colors.black,) : Image.file(_image)),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0, right: 10.0),
-              child: TextFormField(
-                controller: _requestTextEditingController,
-                focusNode: _requestFocusNode,
-                keyboardType: TextInputType.multiline,
-                decoration: const InputDecoration(
-                  hintText: 'Write description . ',
-                  labelText: 'Request description *',
-                  contentPadding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0)
+                  ],
                 ),
               ),
-            )
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width*0.9,
+                height: 120,
+                alignment: Alignment.center,
+                color: Colors.grey,
+                padding: EdgeInsets.all(10.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: _image == null ? Icon(Icons.add, color: Colors.black,) : Image.file(_image)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                child: TextFormField(
+                  controller: _requestTextEditingController,
+                  focusNode: _requestFocusNode,
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                    hintText: 'Write description . ',
+                    labelText: 'Request description *',
+                    contentPadding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0)
+                  ),
+                ),
+              )
 
-          ],
+            ],
+          ),
         ),
       ),
     );
